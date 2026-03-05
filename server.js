16c340e6380d8d5e7c32bddfd78f50f44b0d78b1
JOÃO PEDRO LOPES <joao_pedro-lopes@estudante.sesisenai.org.br>
2026-03-05 12:22:54 -0300

    mensagem do commit

--- server.js
+++ server.js
@@ -1,28 +1,58 @@
 import Fastify from 'fastify'
-import{Pool} from  'pg'
+import { Pool } from 'pg'
 
 const sql = new Pool({
-    user:"postgres",
-    password:"senai",
-    host:"localhost",
-    port:5432,
-    database:"receitas"
+    user: "postgres",
+    password: "senai",
+    host: "localhost",
+    port: 5432,
+    database: "receitas"
 })
 
 const servidor = Fastify();
 
-servidor.get('/usuarios',async()=>{
+servidor.get('/usuarios', async () => {
     const resultado = await sql.query('select * from usuario')
     return resultado.rows
 })
 
 servidor.post('/usuarios', async (request, reply) => {
     const body = request.body
-    const resultado = await sql.query('insert into usuario (nome, senha) values ($1, $2)',[body.nome,body.senha])
+
+    if(!body || !body.nome || !body.senha) {
+        return reply.status(400).send({message:"nome e senha obrigatórios"})
+    }
+
+    const resultado = await sql.query('insert into usuario (nome, senha) values ($1, $2)', [body.nome, body.senha])
     return 'USUARIO CADASTRADO'
+})
+
+servidor.put('/usuarios/:id', async (request, reply) => {
+    const body = request.body
+    const id = request.params.id
+
+    if (!id || !body || !body.nome || !body.senha) {
+        return reply.status(400).send({message:"nome e senha obrigatórios"})
+    }
+
+    const resultado = await sql.query('UPDATE usuario SET nome = $1, senha = $2 WHERE id = $3', [body.nome, body.senha, id])
+    return "USUÁRIO ATUALIZADO"
+})
+
+servidor.delete('/usuarios/:id', async (request, reply) => {
+    const id = request.params.id
+
+    if (!id) {
+        return reply.status(400).send({message:"id obrigatório"})
+    }
+
+    const resultado = await sql.query('DELETE FROM usuario WHERE id = $1', [id])
+    console.log(resultado);
+
+    reply.status(200).send({ message: 'USUÁRIO DELETADO' })
 })
 
 servidor.listen({
     port: 3000
 })
- 
+
